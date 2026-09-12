ba054b708c58c340fea82f5b2e7abe7f1204fbf2
srisquareapp <srisquareconvention@gmail.com>
2026-09-12 10:44:51 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="property360-v3";const A=["./","./index.html","./app.css","./app.js","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
